838a86f2b7e98111ddc6b01d7cd70460197d318f
oucerler <oucerler@gmail.com>
2018-07-26 09:09:24 +0200

    first commit

---
+++ OnlineKassa.java
@@ -0,0 +1,10 @@
+package ItsYaBoy;
+
+public class OnlineKassa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Voorraad.java
@@ -0,0 +1,10 @@
+package ItsYaBoy;
+
+public class Voorraad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
